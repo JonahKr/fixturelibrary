ce9c062305316b80d8f604ebfdda1d326effdf58
Jonah Kraushaar <jonah.kraushaar@gmx.net>
2021-08-19 01:14:01 +0200

    Failstate Handling

--- src/githubhandler.ts
+++ src/githubhandler.ts
@@ -34,16 +34,22 @@ interface GithubTrees {
   truncated: boolean
 }
 
-async function request(url: string): Promise<any> {
-  return fetch(url).then((res) => res.json()).catch((err) => console.error(err));
+async function request(url: string): Promise<object | undefined> {
+  return fetch(url).then(async (data) => {
+    if (data.ok) {
+      return data.json();
+    }
+    console.error(`${data.status} - ${data.statusText}`);
+    return undefined;
+  }).catch((err) => console.error(err));
 }
 
-async function githubRepositoryRequest(endpoint: string): Promise<any> {
+async function githubRepositoryRequest(endpoint: string): Promise<object | any[] | undefined> {
   const url = `https://api.github.com/repos/OpenLightingProject/open-fixture-library/${endpoint}`;
   return request(url);
 }
 
-async function githubRawRequest(endpoint: string): Promise<any> {
+async function githubRawRequest(endpoint: string): Promise<object | undefined> {
   const url = `https://raw.githubusercontent.com/OpenLightingProject/open-fixture-library/${endpoint}`;
   return request(url);
 }
@@ -54,14 +60,15 @@ function extractTagVersion(tag: GithubTag): string {
 
 let latestCommit: string | undefined;
 
-export async function fetchLatestSupportedCommit(forceUpdate = false): Promise<string> {
+export async function fetchLatestSupportedCommit(forceUpdate = false): Promise<string | undefined> {
   if (latestCommit && !forceUpdate) return latestCommit;
-  const tagReq: GithubTag[] = await githubRepositoryRequest('tags');
+  const tagReq = await githubRepositoryRequest('tags') as GithubTag[] | undefined;
+  if (!tagReq) return undefined;
   const version = extractTagVersion(tagReq[0]);
 
   if (version === openFixtureLibraryVersion) {
     // If its equal to the latest supported version, we just fetch the latest commit
-    const latestCommitReq: GithubCommit = await githubRepositoryRequest('commits/master');
+    const latestCommitReq = await githubRepositoryRequest('commits/master') as GithubCommit;
     latestCommit = latestCommitReq.sha;
   } else {
     // Otherwise we just return the sha of the supported version tag.
@@ -80,10 +87,12 @@ export class TruncatedDataError extends Error {
 
 export async function fetchOFLFixtureDirectory(): Promise<any> {
   // At first we get the tree sha for the fixtures directory
-  const dirReq: GithubTrees = await githubRepositoryRequest(`git/trees/${await fetchLatestSupportedCommit()}`);
+  const latComm = await fetchLatestSupportedCommit();
+  if (!latComm) return undefined;
+  const dirReq = await githubRepositoryRequest(`git/trees/${latComm}`) as GithubTrees;
   const fixturesDirSha = dirReq.tree.filter((e) => (e.path.startsWith('fixtures') && e.type === 'tree'))[0].sha;
   // Now we can continue with the sub-tree
-  const fixtReq: GithubTrees = await githubRepositoryRequest(`git/trees/${fixturesDirSha}?recursive=1`);
+  const fixtReq = await githubRepositoryRequest(`git/trees/${fixturesDirSha}?recursive=1`) as GithubTrees;
   // Before we continue we need to check if the data got truncated
   if (fixtReq.truncated) throw new TruncatedDataError('The Open Fixture Library got to big. Please resort to specific File downloading!');
 
@@ -96,10 +105,12 @@ export async function fetchOFLFixtureDirectory(): Promise<any> {
   return fixtures;
 }
 
-export async function fetchOFLFixture(path: string): Promise<Fixture> {
+export async function fetchOFLFixture(path: string): Promise<Fixture | undefined> {
   let corrPath = path;
   if (!path.endsWith('.json')) {
     corrPath = `${path}.json`;
   }
-  return await githubRawRequest(`${await fetchLatestSupportedCommit()}/fixtures/${corrPath}`) as Fixture;
+  const latComm = await fetchLatestSupportedCommit();
+  if (!latComm) return undefined;
+  return await githubRawRequest(`${latComm}/fixtures/${corrPath}`) as Fixture;
 }
