6a134bdb04ca1b42503c0efc88c536d39dbd9b24
Jonah Kraushaar <jonah.kraushaar@gmx.net>
2021-08-18 17:34:24 +0200

    type documentation

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,16 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import * as types from './types';
 
+/**
+ * Type definitions for the Open Fixture Library fixture schema.
+ *
+ * Most important types:
+ * - Fixture: {@link Fixture}
+ * - Capabilities: {@link Capabilities}
+ */
 export import Types = types;
 
-export { FixtureIndex, IndexItem, parseFixtureKey } from './fixtureindex';
+export { FixtureIndex, IndexItem } from './fixtureindex';
 export { LocalStorageFixtureIndex } from './localstoragefixtureindex';
 
 export { TruncatedDataError, fetchOFLFixtureDirectory, fetchOFLFixture } from './githubhandler';
